37206c82d205ca52c1bb3ec01d4268c68570fa91
hafen <rhafen@gmail.com>
2018-09-20 16:18:34 -0700

    Remove logging in production build

--- src/store.js
+++ src/store.js
@@ -3,11 +3,17 @@ import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import rootReducer from './reducers';
 
+let middleware = [thunkMiddleware];
+
+if (process.env.NODE_ENV !== 'production') {
+  middleware = [...middleware, createLogger()];
+}
+
 const store = createStore(
   rootReducer,
   // { initial state... },
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  applyMiddleware(thunkMiddleware, createLogger())
+  applyMiddleware(...middleware)
 );
 
 export default store;
